b999beb3f69e6d60aae86925d619e038a42b121e
Mikaela Scornaienchi <scornaim@gmail.com>
2018-05-24 15:01:18 -0400

    working CALLLgit add -Agit add -A :D

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
-import EventsInTown from './EventsInTown';
+// import EventsInTown from './EventsInTown';
 
 class App extends React.Component {
   
@@ -16,36 +16,55 @@ class App extends React.Component {
   }
 
 
+
   handleChange(e) {
     console.log(e);
-    
     this.setState({
-      // value: event.target.value
       [e.target.name]: e.target.value
-    })
+    });
 
-    // store the value as theArtist
   }
   handleSubmit(e) {
     e.preventDefault();
-    console.log('handleSubmit is running!');
+    let theArtist = ''
+    theArtist = this.state.artistName;
+    console.log(theArtist);
+    // console.log('the artist');
+
+    // componentDidMount() {
+
+      // this.state.props.artistName
+      // let theArtist = ''
+      // theArtist = this.state.artistName;
+      // console.log(theArtist);
+      // console.log('the artist');
+      axios({
+        url: `https://rest.bandsintown.com/artists/${theArtist}/events/`,
+        params: {
+          app_id: `6e7ce2bb9f77b677bc181759630ddcf4`
+        }
+      })
+        .then((res) => {
+          console.log('yes');
+          console.log(res)
+        });
+    // }
   }
 
-    render() {
-      return (
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <input required type="text" name="artistName" value={this.state.artistName} onChange={this.handleChange}  />
-            {/* <select name="" id="">
-              <option value=""></option>
-              <option value=""></option>
-            </select> */}
-            <input type="submit" value="submit" />
-          <EventsInTown />
-          </form>
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input required type="text" name="artistName" value={this.state.artistName} onChange={this.handleChange}  />
+          {/* <select name="" id="">
+            <option value=""></option>
+            <option value=""></option>
+          </select> */}
+          <input type="submit" value="Artist Search" />
+        </form>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
