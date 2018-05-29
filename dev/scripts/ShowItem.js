ef1378440db99739efbefcb4c9553142c1111010
Clint Lee <l.clint91@gmail.com>
2018-05-29 17:00:27 -0400

    merging for styles

--- dev/scripts/ShowItem.js
+++ dev/scripts/ShowItem.js
@@ -3,9 +3,12 @@ import React from 'react';
 const ShowItem = (props) => {
   return(
     <li className="showItems" >
-      <p className="showDate showItem">Date of Concert: {props.date}</p>
-      <p className="showVenue showItem">Concert Venue: {props.venue}</p>
-      <p className="showCity showItem">City of Concert: {props.city}</p>
+      <p className="showDate showItem">Date of Concert:</p>
+      <p className="showDate showItem">{props.date}</p>
+      <p className="showVenue showItem">Concert Venue:</p>
+      <p className="showVenue showItem">{props.venue}</p>
+      <p className="showCity showItem">City of Concert:</p>
+      <p className="showCity showItem">{props.city}</p>
       <a className="showTickets showItem" href={props.url}>Buy Tickets!</a>
     </li>
   )
